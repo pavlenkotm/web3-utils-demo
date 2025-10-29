eabdc88ab6212a87761ebfa15808004312d6111d
Andrey Pavlenko <31548235+pavlenkotm@users.noreply.github.com>
2025-10-29 23:17:20 +0300

    Create demo_contract.sol

---
+++ demo_contract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DemoContract {
+    string public author = "PavlenkoTM";
+
+    function hello() public pure returns (string memory) {
+        return "Hello from Web3 builder PavlenkoTM";
+    }
+}
